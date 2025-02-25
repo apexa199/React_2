c282b5d29b12ccc1a004ea93e12b56b737159c96
apexa199 <apexaajudiya9@gmail.com>
2025-02-25 19:08:35 +0530

    button click

--- my-react-project/src/App.jsx
+++ my-react-project/src/App.jsx
@@ -5,51 +5,58 @@ import Profile from './Profile/Profile'
 function App() {
   const Users = [
     {
-        "id": 1,
-        "name": "Leanne Graham",
-        "username": "Bret",
-        "email": "Sincere@april.biz",
-        "phone": "1-770-736-8031 x56442",
-        "website": "hildegard.org",
+      "id": 1,
+      "name": "Leanne Graham",
+      "username": "Bret",
+      "email": "Sincere@april.biz",
+      "phone": "1-770-736-8031 x56442",
+      "website": "hildegard.org",
     },
     {
-        "id": 2,
-        "name": "",
-        "username": "",
-        "email": "",
-        "phone": "",
-        "website": "",
+      "id": 2,
+      "name": "",
+      "username": "",
+      "email": "",
+      "phone": "",
+      "website": "",
     },
     {
-        "id": 3,
-        "name": "Clementine Bauch",
-        "username": "Samantha",
-        "email": "Nathan@yesenia.net",
-        "phone": "1-463-123-4447",
-        "website": "ramiro.info",
+      "id": 3,
+      "name": "Clementine Bauch",
+      "username": "Samantha",
+      "email": "Nathan@yesenia.net",
+      "phone": "1-463-123-4447",
+      "website": "ramiro.info",
     },
     {
-        "id": 4,
-        "name": "Patricia Lebsack",
-        "username": "Karianne",
-        "email": "Julianne.OConner@kory.org",
-        "phone": "493-170-9623 x156",
-        "website": "kale.biz",
+      "id": 4,
+      "name": "Patricia Lebsack",
+      "username": "Karianne",
+      "email": "Julianne.OConner@kory.org",
+      "phone": "493-170-9623 x156",
+      "website": "kale.biz",
     },
     {
-        "id": 5,
-        "name": "Chelsey Dietrich",
-        "username": "Kamren",
-        "email": "Lucio_Hettinger@annie.ca",
-        "phone": "(254)954-1289",
-        "website": "demarco.info",
+      "id": 5,
+      "name": "Chelsey Dietrich",
+      "username": "Kamren",
+      "email": "Lucio_Hettinger@annie.ca",
+      "phone": "(254)954-1289",
+      "website": "demarco.info",
     },
-]
+  ]
 
+  const handleClick = (something) => {
+    console.log(`Button Clicked`);
+
+
+  }
   return (
     <>
       <h2>Users Details  </h2>
-      <Profile users={Users}/>
+      <Profile users={Users} />
+      <button onClick={() => handleClick("First Button Clicked.")}>First Button</button>
+      <button onClick={() => handleClick("Second Button Clicked.")}>Second Button</button>
 
     </>
   )
